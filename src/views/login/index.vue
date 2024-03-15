fb463924d2bfd1638e2c9e6af5adb6e1eab567d5
garlam <garlam_au@qq.com>
2024-03-16 02:29:35 +0800

    update

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -8,7 +8,7 @@
         <!--        </div>-->
         <div class="view-account-top-desc">
           <n-icon color="#69AA46">
-            <leaf />
+            <leaf/>
           </n-icon>
           <span class="red"> ACE </span>
           <span class="white"> APPLICATION </span>
@@ -21,13 +21,12 @@
           label-placement="left"
           size="large"
           :model="formInline"
-          :rules="rules"
-        >
+          :rules="rules">
           <n-form-item path="username">
             <n-input v-model:value="formInline.username" placeholder="请输入用户名">
               <template #prefix>
                 <n-icon size="18" color="#808695">
-                  <PersonOutline />
+                  <PersonOutline/>
                 </n-icon>
               </template>
             </n-input>
@@ -37,11 +36,10 @@
               v-model:value="formInline.password"
               type="password"
               showPasswordOn="click"
-              placeholder="请输入密码"
-            >
+              placeholder="请输入密码">
               <template #prefix>
                 <n-icon size="18" color="#808695">
-                  <LockClosedOutline />
+                  <LockClosedOutline/>
                 </n-icon>
               </template>
             </n-input>
@@ -71,28 +69,28 @@
               <div class="flex-initial mx-2">
                 <a href="javascript:">
                   <n-icon size="24" color="#fff">
-                    <LogoGithub />
+                    <LogoGithub/>
                   </n-icon>
                 </a>
               </div>
               <div class="flex-initial mx-2">
                 <a href="javascript:">
                   <n-icon size="24" color="#2d8cf0">
-                    <LogoFacebook />
+                    <LogoFacebook/>
                   </n-icon>
                 </a>
               </div>
               <div class="flex-initial mx-2">
                 <a href="javascript:">
                   <n-icon size="24" color="#f77">
-                    <LogoGoogle />
+                    <LogoGoogle/>
                   </n-icon>
                 </a>
               </div>
               <div class="flex-initial mx-2">
                 <a href="javascript:">
                   <n-icon size="24" color="#69AA46">
-                    <LogoWechat />
+                    <LogoWechat/>
                   </n-icon>
                 </a>
               </div>
@@ -108,148 +106,153 @@
 </template>
 
 <script lang="ts" setup>
-  import { reactive, ref } from 'vue';
-  import { useRoute, useRouter } from 'vue-router';
-  import { useUserStore } from '@/store/modules/user';
-  import { useMessage } from 'naive-ui';
-  import { ResultEnum } from '@/enums/httpEnum';
-  import {
-    PersonOutline,
-    LockClosedOutline,
-    LogoGithub,
-    LogoFacebook,
-    Leaf,
-    LogoWechat,
-    LogoGoogle,
-  } from '@vicons/ionicons5';
-  import { PageEnum } from '@/enums/pageEnum';
-  import { websiteConfig } from '@/config/website.config';
-  interface FormState {
-    username: string;
-    password: string;
-  }
+import {reactive, ref} from 'vue';
+import {useRoute, useRouter} from 'vue-router';
+import {useUserStore} from '@/store/modules/user';
+import {useMessage} from 'naive-ui';
+import {ResultEnum} from '@/enums/httpEnum';
+import {
+  PersonOutline,
+  LockClosedOutline,
+  LogoGithub,
+  LogoFacebook,
+  Leaf,
+  LogoWechat,
+  LogoGoogle,
+} from '@vicons/ionicons5';
+import {PageEnum} from '@/enums/pageEnum';
+import {websiteConfig} from '@/config/website.config';
 
-  const formRef = ref();
-  const message = useMessage();
-  const loading = ref(false);
-  const autoLogin = ref(false);
-  const LOGIN_NAME = PageEnum.BASE_LOGIN_NAME;
+interface FormState {
+  username: string;
+  password: string;
+}
 
-  const formInline = reactive({
-    username: 'admin',
-    password: '123456',
-    isCaptcha: true,
-  });
+const formRef = ref();
+const message = useMessage();
+const loading = ref(false);
+const autoLogin = ref(false);
+const LOGIN_NAME = PageEnum.BASE_LOGIN_NAME;
 
-  const rules = {
-    username: { required: true, message: '请输入用户名', trigger: 'blur' },
-    password: { required: true, message: '请输入密码', trigger: 'blur' },
-  };
+const formInline = reactive({
+  username: 'admin',
+  password: '123456',
+  isCaptcha: true,
+});
 
-  const userStore = useUserStore();
+const rules = {
+  username: {required: true, message: '请输入用户名', trigger: 'blur'},
+  password: {required: true, message: '请输入密码', trigger: 'blur'},
+};
 
-  const router = useRouter();
-  const route = useRoute();
+const userStore = useUserStore();
 
-  const handleSubmit = (e: { preventDefault: () => void }) => {
-    e.preventDefault();
-    formRef.value.validate(async (errors: any) => {
-      if (!errors) {
-        const { username, password } = formInline;
-        message.loading('登录中...');
-        loading.value = true;
+const router = useRouter();
+const route = useRoute();
 
-        const params: FormState = {
-          username,
-          password,
-        };
+const handleSubmit = (e: { preventDefault: () => void }) => {
+  e.preventDefault();
+  formRef.value.validate(async (errors: any) => {
+    if (!errors) {
+      const {username, password} = formInline;
+      message.loading('登录中...');
+      loading.value = true;
 
-        try {
-          const { code, message: msg } = await userStore.login(params);
-          message.destroyAll();
-          if (code == ResultEnum.SUCCESS) {
-            const toPath = decodeURIComponent((route.query?.redirect || '/') as string);
-            message.success('登录成功，即将进入系统');
-            if (route.name === LOGIN_NAME) {
-              await router.replace('/');
-            } else await router.replace(toPath);
-          } else {
-            message.info(msg || '登录失败');
-          }
-        } finally {
-          loading.value = false;
+      const params: FormState = {
+        username,
+        password,
+      };
+
+      try {
+        const {code, message: msg} = await userStore.login(params);
+        message.destroyAll();
+        if (code == ResultEnum.SUCCESS) {
+          const toPath = decodeURIComponent((route.query?.redirect || '/') as string);
+          message.success('登录成功，即将进入系统');
+          if (route.name === LOGIN_NAME) {
+            await router.replace('/');
+          } else await router.replace(toPath);
+        } else {
+          message.info(msg || '登录失败');
         }
-      } else {
-        message.error('请填写完整信息，并且进行验证码校验');
+      } finally {
+        loading.value = false;
       }
-    });
-  };
+    } else {
+      message.error('请填写完整信息，并且进行验证码校验');
+    }
+  });
+};
 </script>
 
 <style lang="less" scoped>
-  .view-account {
-    display: flex;
-    flex-direction: column;
-    height: 100vh;
-    overflow: auto;
-    background-color: #1d2024;
+.view-account {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  overflow: auto;
+  background-color: #1d2024;
 
-    &-container {
-      flex: 1;
-      padding: 32px 12px;
-      max-width: 384px;
-      min-width: 320px;
-      margin: 0 auto;
+  &-container {
+    flex: 1;
+    padding: 32px 12px;
+    max-width: 384px;
+    min-width: 320px;
+    margin: 0 auto;
+  }
+
+  &-top {
+    padding: 32px 0;
+    text-align: center;
+
+    &-desc {
+      font-size: 35px;
     }
 
-    &-top {
-      padding: 32px 0;
-      text-align: center;
-
-      &-desc {
-        font-size: 35px;
-      }
-
-      &-trademark {
-        font-size: 20px;
-        color: #478fca;
-        font-weight: bold;
-      }
-    }
-
-    &-other {
-      width: 100%;
-    }
-
-    .default-color {
-      color: #fff;
+    &-trademark {
+      font-size: 20px;
+      color: #478fca;
       font-weight: bold;
-      font-size: 16px;
-      .ant-checkbox-wrapper {
-        color: #515a6e;
-      }
     }
   }
 
-  @media (min-width: 768px) {
-    .view-account {
-      background-image: url('../../assets/images/login.jpg');
-      background-repeat: no-repeat;
-      background-position: 50%;
-      background-size: 100%;
-    }
+  &-other {
+    width: 100%;
+  }
 
-    .page-account-container {
-      padding: 32px 0 24px 0;
-    }
-  }
-  .red {
-    color: #f77;
-  }
-  .white {
+  .default-color {
     color: #fff;
+    font-weight: bold;
+    font-size: 16px;
+
+    .ant-checkbox-wrapper {
+      color: #515a6e;
+    }
   }
-  .green {
-    color: #69aa46;
+}
+
+@media (min-width: 768px) {
+  .view-account {
+    background-image: url('../../assets/images/login.jpg');
+    background-repeat: no-repeat;
+    background-position: 50%;
+    background-size: 100%;
   }
+
+  .page-account-container {
+    padding: 32px 0 24px 0;
+  }
+}
+
+.red {
+  color: #f77;
+}
+
+.white {
+  color: #fff;
+}
+
+.green {
+  color: #69aa46;
+}
 </style>
