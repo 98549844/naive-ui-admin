3da70c6b05ee846b23241d19a857ed57e9725091
garlam <garlam_au@qq.com>
2024-05-19 11:20:30 +0800

    update

--- src/layout/components/Logo/index.vue
+++ src/layout/components/Logo/index.vue
@@ -1,49 +1,50 @@
 <template>
   <div class="logo">
-    <!--    <img :src="websiteConfig.logo" alt="" :class="{ 'mr-2': !collapsed }" />-->
+    <!--<img :src="websiteConfig.logo" alt="" :class="{ 'mr-2': !collapsed }"/>-->
     <n-icon color="#69AA46" size="25">
-      <leaf />
+      <leaf/>
     </n-icon>
     <h2 v-show="!collapsed" class="title">&nbsp;&nbsp;{{ websiteConfig.title }}</h2>
   </div>
 </template>
 
 <script lang="ts">
-  import { websiteConfig } from '@/config/website.config';
-  import { Leaf } from '@vicons/ionicons5';
-  export default {
-    name: 'Index',
-    components: { Leaf },
-    props: {
-      collapsed: {
-        type: Boolean,
-      },
+import {websiteConfig} from '@/config/website.config';
+import {Leaf} from '@vicons/ionicons5';
+
+export default {
+  name: 'Index',
+  components: {Leaf},
+  props: {
+    collapsed: {
+      type: Boolean,
     },
-    data() {
-      return {
-        websiteConfig,
-      };
-    },
-  };
+  },
+  data() {
+    return {
+      websiteConfig,
+    };
+  },
+};
 </script>
 
 <style lang="less" scoped>
-  .logo {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 64px;
-    line-height: 64px;
-    overflow: hidden;
-    white-space: nowrap;
+.logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 64px;
+  line-height: 64px;
+  overflow: hidden;
+  white-space: nowrap;
 
-    img {
-      width: auto;
-      height: 32px;
-    }
-
-    .title {
-      margin: 0;
-    }
+  img {
+    width: auto;
+    height: 32px;
   }
+
+  .title {
+    margin: 0;
+  }
+}
 </style>
